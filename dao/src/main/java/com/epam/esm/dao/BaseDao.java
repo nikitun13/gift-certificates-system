b5f78642054068a6f23fbd1f6398baadef06e8c8
Nikita Romanov <nikitun13@gmail.com>
2022-01-14 13:47:10 +0300

    add base dao interface

---
+++ dao/src/main/java/com/epam/esm/dao/BaseDao.java
@@ -0,0 +1,44 @@
+package com.epam.esm.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseDao<K, E> {
+
+    /**
+     * Returns all {@code entities} from the repository.
+     *
+     * @return list of the all {@code entities} from the repository.
+     */
+    List<E> findAll();
+
+    /**
+     * Finds {@code entity} by its {@code id}.
+     *
+     * @param id {@code id} of the {@code entity}.
+     * @return Optional {@code entity}. If the given {@code id} exists,
+     * {@link Optional} contains corresponding entity, empty {@link Optional} otherwise.
+     */
+    Optional<E> findById(K id);
+
+    /**
+     * Adds new {@code entity} to the repository.
+     *
+     * @param entity {@code entity} to be added to the repository.
+     */
+    void create(E entity);
+
+    /**
+     * Updates {@code entity} in the repository.
+     *
+     * @param entity {@code entity} to be updated in the repository.
+     */
+    void update(E entity);
+
+    /**
+     * Deletes {@code entity} from the repository by its {@code id}.
+     *
+     * @param id of the {@code entity} to be deleted.
+     */
+    void delete(K id);
+}
