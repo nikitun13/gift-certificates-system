e26f6a19ce15ceefbb55349e6cfc9d253edd6cc8
Nikita Romanov <nikitun13@gmail.com>
2022-01-18 14:30:26 +0300

    add GiftCertificateService and its impl

---
+++ service/src/main/java/com/epam/esm/service/GiftCertificateService.java
@@ -0,0 +1,56 @@
+package com.epam.esm.service;
+
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.UpdateGiftCertificateDto;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Describes interface of the service that provides
+ * CRUD operations with {@code GiftCertificate} entities.
+ */
+public interface GiftCertificateService {
+
+    /**
+     * Finds all entities from the storage and maps them
+     * to {@link GiftCertificateDto}.
+     *
+     * @return list of all {@code GiftCertificateDtos}.
+     */
+    List<GiftCertificateDto> findAll();
+
+    /**
+     * Finds entity by id and maps it to {@link GiftCertificateDto}.
+     * If no such id returns empty {@link Optional}.
+     *
+     * @param id {@code id} of the entity.
+     * @return Optional {@code GiftCertificateDto}.
+     */
+    Optional<GiftCertificateDto> findById(Long id);
+
+    /**
+     * Updates {@code GiftCertificate} in the storage.
+     * All fields are optional, updates only required fields.
+     *
+     * @param updateGiftCertificateDto dto for update.
+     */
+    void update(UpdateGiftCertificateDto updateGiftCertificateDto);
+
+    /**
+     * Deletes entity by its {@code id}.
+     *
+     * @param id {@code id} of the entity.
+     */
+    void delete(Long id);
+
+    /**
+     * Creates new {@code GiftCertificate} in the storage.
+     * Uses {@link UpdateGiftCertificateDto} for creation,
+     * all fields are required (except {@code id}).
+     *
+     * @param createGiftCertificateDto {@code dto} for creation new entity.
+     * @return created entity that mapped to {@link GiftCertificateDto}.
+     */
+    GiftCertificateDto create(UpdateGiftCertificateDto createGiftCertificateDto);
+}
---
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -0,0 +1,68 @@
+package com.epam.esm.service.impl;
+
+import com.epam.esm.dao.GiftCertificateDao;
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.UpdateGiftCertificateDto;
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.mapper.Mapper;
+import com.epam.esm.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GiftCertificateServiceImpl implements GiftCertificateService {
+
+    private final GiftCertificateDao giftCertificateDao;
+    private final Mapper<GiftCertificate, GiftCertificateDto> giftCertificateDtoMapper;
+    private final Mapper<GiftCertificate, UpdateGiftCertificateDto> updateGiftCertificateDtoMapper;
+
+    @Autowired
+    public GiftCertificateServiceImpl(GiftCertificateDao giftCertificateDao,
+                                      Mapper<GiftCertificate, GiftCertificateDto> giftCertificateDtoMapper,
+                                      Mapper<GiftCertificate, UpdateGiftCertificateDto> updateGiftCertificateDtoMapper) {
+        this.giftCertificateDao = giftCertificateDao;
+        this.giftCertificateDtoMapper = giftCertificateDtoMapper;
+        this.updateGiftCertificateDtoMapper = updateGiftCertificateDtoMapper;
+    }
+
+    @Override
+    public List<GiftCertificateDto> findAll() {
+        return giftCertificateDao.findAll()
+                .stream()
+                .map(giftCertificateDtoMapper::mapToDto)
+                .toList();
+    }
+
+    @Override
+    public Optional<GiftCertificateDto> findById(Long id) {
+        return giftCertificateDao.findById(id)
+                .map(giftCertificateDtoMapper::mapToDto);
+    }
+
+    @Override
+    public void update(UpdateGiftCertificateDto updateGiftCertificateDto) {
+        GiftCertificate entity = updateGiftCertificateDtoMapper.mapToEntity(updateGiftCertificateDto);
+        LocalDateTime now = LocalDateTime.now();
+        entity.setLastUpdateDate(now);
+        giftCertificateDao.update(entity);
+    }
+
+    @Override
+    public void delete(Long id) {
+        giftCertificateDao.delete(id);
+    }
+
+    @Override
+    public GiftCertificateDto create(UpdateGiftCertificateDto createGiftCertificateDto) {
+        GiftCertificate entity = updateGiftCertificateDtoMapper.mapToEntity(createGiftCertificateDto);
+        LocalDateTime now = LocalDateTime.now();
+        entity.setCreateDate(now);
+        entity.setLastUpdateDate(now);
+        giftCertificateDao.create(entity);
+        return giftCertificateDtoMapper.mapToDto(entity);
+    }
+}
