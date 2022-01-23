92327a006f99eb6b5aa475b9491e0aade19713ca
Nikita Romanov <nikitun13@gmail.com>
2022-01-23 18:09:47 +0300

    update GiftCertificateTagService and its impl

--- service/src/main/java/com/epam/esm/service/GiftCertificateTagService.java
+++ service/src/main/java/com/epam/esm/service/GiftCertificateTagService.java
@@ -1,6 +1,7 @@
 package com.epam.esm.service;
 
 import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.dto.TagDto;
 import com.epam.esm.dto.UpdateGiftCertificateDto;
 
 import java.util.List;
@@ -14,6 +15,13 @@ import java.util.Map;
  * @see TagService
  */
 public interface GiftCertificateTagService {
+
+    /**
+     * Finds all {@code GiftCertificates} with {@code Tags} from the storage.
+     *
+     * @return all {@code GiftCertificates} with {@code Tags}.
+     */
+    List<GiftCertificateDto> findAll();
 
     /**
      * Updates {@code GiftCertificate}. Creates many-to-many relationship
@@ -32,5 +40,15 @@ public interface GiftCertificateTagService {
      */
     GiftCertificateDto create(UpdateGiftCertificateDto createGiftCertificateDto);
 
+    /**
+     * Finds entities by different params and orders by the given fields.
+     * Unknown params in the list of parameters will be ignored.
+     * Sets {@code Tags} to the found {@code Certificates}.
+     *
+     * @param params  filtering properties of the {@code certificate} and {@code tag}.
+     * @param orderBy list of the ordering by fields.
+     * @return list of the {@link GiftCertificateDto} that satisfy all the filters
+     * with the list of {@link TagDto}.
+     */
     List<GiftCertificateDto> findGiftCertificatesByParams(Map<String, String> params, List<String> orderBy);
 }
--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateTagServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateTagServiceImpl.java
@@ -4,18 +4,28 @@ import com.epam.esm.dao.GiftCertificateTagDao;
 import com.epam.esm.dto.GiftCertificateDto;
 import com.epam.esm.dto.TagDto;
 import com.epam.esm.dto.UpdateGiftCertificateDto;
+import com.epam.esm.entity.Tag;
 import com.epam.esm.service.GiftCertificateService;
 import com.epam.esm.service.GiftCertificateTagService;
 import com.epam.esm.service.TagService;
+import com.epam.esm.util.ParamParseUtil;
+import com.epam.esm.util.ReflectUtil;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.beans.Introspector;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toMap;
+
 @Service
 public class GiftCertificateTagServiceImpl implements GiftCertificateTagService {
+
+    private static final String TAG_PARAMS_PREFIX = "tag";
 
     private final GiftCertificateService giftCertificateService;
     private final TagService tagService;
@@ -28,6 +38,13 @@ public class GiftCertificateTagServiceImpl implements GiftCertificateTagService 
         this.giftCertificateService = giftCertificateService;
         this.tagService = tagService;
         this.giftCertificateTagDao = giftCertificateTagDao;
+    }
+
+    @Override
+    public List<GiftCertificateDto> findAll() {
+        List<GiftCertificateDto> certificates = giftCertificateService.findAll();
+        certificates.forEach(this::setTagsToCertificate);
+        return certificates;
     }
 
     @Override
@@ -58,11 +75,27 @@ public class GiftCertificateTagServiceImpl implements GiftCertificateTagService 
     @Override
     public List<GiftCertificateDto> findGiftCertificatesByParams(Map<String, String> params,
                                                                  List<String> orderBy) {
-        List<GiftCertificateDto> certificates = giftCertificateService.findByParams(params, orderBy);
-        for (GiftCertificateDto certificate : certificates) {
-            List<TagDto> tags = tagService.findByGiftCertificate(certificate);
-            certificate.tags().addAll(tags);
+        if (ObjectUtils.isNotEmpty(params) || ObjectUtils.isNotEmpty(orderBy)) {
+            Map<String, String> tagProperties = params.entrySet().stream()
+                    .filter(entry -> entry.getKey().startsWith(TAG_PARAMS_PREFIX))
+                    .map(entry -> Pair.of(
+                            Introspector.decapitalize(entry.getKey().substring(TAG_PARAMS_PREFIX.length())),
+                            entry.getValue()
+                    ))
+                    .filter(pair -> ReflectUtil.isContainsField(Tag.class,
+                            ParamParseUtil.removeOperationCharacterIfPresent(pair.getLeft())
+                    ))
+                    .collect(toMap(Pair::getLeft, Pair::getRight));
+            List<GiftCertificateDto> certificates = giftCertificateService.findByParams(params, tagProperties, orderBy);
+            certificates.forEach(this::setTagsToCertificate);
+            return certificates;
+        } else {
+            return findAll();
         }
-        return certificates;
+    }
+
+    private void setTagsToCertificate(GiftCertificateDto certificate) {
+        List<TagDto> tags = tagService.findByGiftCertificate(certificate);
+        certificate.tags().addAll(tags);
     }
 }
