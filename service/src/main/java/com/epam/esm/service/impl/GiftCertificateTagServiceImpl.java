4aeebe0aa47e87b4e03f4049f70b46726a493dfe
Nikita Romanov <nikitun13@gmail.com>
2022-01-24 15:57:31 +0300

    fix bug

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateTagServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateTagServiceImpl.java
@@ -1,6 +1,7 @@
 package com.epam.esm.service.impl;
 
 import com.epam.esm.dao.GiftCertificateTagDao;
+import com.epam.esm.dto.CreateTagDto;
 import com.epam.esm.dto.GiftCertificateDto;
 import com.epam.esm.dto.TagDto;
 import com.epam.esm.dto.UpdateGiftCertificateDto;
@@ -59,10 +60,13 @@ public class GiftCertificateTagServiceImpl implements GiftCertificateTagService 
     @Transactional
     public void update(UpdateGiftCertificateDto updateGiftCertificateDto, Long giftCertificateId) {
         giftCertificateService.update(updateGiftCertificateDto, giftCertificateId);
-        updateGiftCertificateDto.tags().stream()
-                .map(tagService::createIfNotExists)
-                .map(TagDto::id)
-                .forEach(id -> giftCertificateTagDao.create(giftCertificateId, id));
+        List<CreateTagDto> tags = updateGiftCertificateDto.tags();
+        if (ObjectUtils.isNotEmpty(tags)) {
+            tags.stream()
+                    .map(tagService::createIfNotExists)
+                    .map(TagDto::id)
+                    .forEach(id -> giftCertificateTagDao.create(giftCertificateId, id));
+        }
     }
 
     @Override
@@ -70,13 +74,16 @@ public class GiftCertificateTagServiceImpl implements GiftCertificateTagService 
     public GiftCertificateDto create(UpdateGiftCertificateDto createGiftCertificateDto) {
         GiftCertificateDto createdGiftCertificate = giftCertificateService.create(createGiftCertificateDto);
         Long giftCertificateId = createdGiftCertificate.id();
-        List<TagDto> tags = createGiftCertificateDto.tags().stream()
-                .map(tagService::createIfNotExists)
-                .toList();
-        tags.stream()
-                .map(TagDto::id)
-                .forEach(id -> giftCertificateTagDao.create(giftCertificateId, id));
-        createdGiftCertificate.tags().addAll(tags);
+        List<CreateTagDto> createTags = createGiftCertificateDto.tags();
+        if (ObjectUtils.isNotEmpty(createTags)) {
+            List<TagDto> tags = createTags.stream()
+                    .map(tagService::createIfNotExists)
+                    .toList();
+            tags.stream()
+                    .map(TagDto::id)
+                    .forEach(id -> giftCertificateTagDao.create(giftCertificateId, id));
+            createdGiftCertificate.tags().addAll(tags);
+        }
         return createdGiftCertificate;
     }
 
