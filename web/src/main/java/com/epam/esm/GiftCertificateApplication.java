2c7ca4005e289f465bd7539223c364cd9fc3ed69
Nikita Romanov <nikitun13@gmail.com>
2022-02-05 19:22:33 +0300

    migrate to spring boot - add SpringBootApplication main class

---
+++ web/src/main/java/com/epam/esm/GiftCertificateApplication.java
@@ -0,0 +1,12 @@
+package com.epam.esm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCertificateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftCertificateApplication.class, args);
+    }
+}
