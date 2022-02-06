2a4d68c01cfec97b8a5528eeaecac6053bd44f43
Nikita Romanov <nikitun13@gmail.com>
2022-02-06 17:21:15 +0300

    update ServiceTestConfig

--- service/src/test/java/com/epam/esm/service/config/ServiceTestConfig.java
+++ service/src/test/java/com/epam/esm/service/config/ServiceTestConfig.java
@@ -1,15 +1,16 @@
-package com.epam.esm.service;
+package com.epam.esm.service.config;
 
 import com.epam.esm.dao.GiftCertificateDao;
-import com.epam.esm.dao.GiftCertificateTagDao;
 import com.epam.esm.dao.TagDao;
+import com.epam.esm.service.GiftCertificateService;
+import com.epam.esm.service.TagService;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(value = "com.epam.esm.mapper", lazyInit = true)
+@ComponentScan(value = "com.epam.esm.mapper")
 public class ServiceTestConfig {
 
     @Bean
@@ -20,11 +21,6 @@ public class ServiceTestConfig {
     @Bean
     public TagDao tagDao() {
         return Mockito.mock(TagDao.class);
-    }
-
-    @Bean
-    public GiftCertificateTagDao giftCertificateTagDao() {
-        return Mockito.mock(GiftCertificateTagDao.class);
     }
 
     @Bean
