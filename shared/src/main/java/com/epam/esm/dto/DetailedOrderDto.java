f60c204683a65016ac0fedb4295ad919d899a30e
Nikita Romanov <nikitun13@gmail.com>
2022-02-15 00:36:11 +0300

    add lastUpdateDate field

--- shared/src/main/java/com/epam/esm/dto/DetailedOrderDto.java
+++ shared/src/main/java/com/epam/esm/dto/DetailedOrderDto.java
@@ -8,6 +8,8 @@ import java.util.List;
 public record DetailedOrderDto(Long id,
                                @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
                                LocalDateTime createDate,
+                               @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+                               LocalDateTime lastUpdateDate,
                                Long totalPrice,
                                List<OrderDetailDto> details) {
 }
--- shared/src/main/java/com/epam/esm/dto/OrderDto.java
+++ shared/src/main/java/com/epam/esm/dto/OrderDto.java
@@ -7,5 +7,7 @@ import java.time.LocalDateTime;
 public record OrderDto(Long id,
                        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
                        LocalDateTime createDate,
+                       @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+                       LocalDateTime lastUpdateDate,
                        Long totalPrice) {
 }
