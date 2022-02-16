fc5a8eb29bfd13cfd7adeccc1da68a8b5cd7e528
Nikita Romanov <nikitun13@gmail.com>
2022-02-16 22:12:57 +0300

    update GiftCertificateDaoImplTest

--- persistence/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
+++ persistence/src/test/java/com/epam/esm/dao/impl/GiftCertificateDaoImplTest.java
@@ -2,16 +2,21 @@ package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.GiftCertificateDao;
 import com.epam.esm.dao.config.DaoTestConfig;
+import com.epam.esm.dto.GiftCertificateFilters;
 import com.epam.esm.entity.GiftCertificate;
-import com.epam.esm.dto.Page;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,88 +24,84 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = DaoTestConfig.class)
 @Transactional
+@ActiveProfiles("test")
 class GiftCertificateDaoImplTest {
 
-    private GiftCertificate cocaColaCertificate;
-    private GiftCertificate kfcCertificate;
-    private GiftCertificate oneHundredDollarsCertificate;
-    private GiftCertificate NoTagsCertificate;
+    private static final PageRequest DEFAULT_PAGEABLE = PageRequest.of(0, 20);
 
-    @BeforeEach
-    void init() {
-        cocaColaCertificate = GiftCertificate.builder()
-                .id(1L)
-                .name("Free coca-cola certificate")
-                .description("Simple use it and get coca-cola")
-                .price(1000L)
-                .duration(10)
-                .createDate(LocalDateTime.parse("2022-01-01T12:00:00"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-02T13:30:25"))
-                .build();
-
-        kfcCertificate = GiftCertificate.builder()
-                .id(2L)
-                .name("KFC certificate")
-                .description("10 dollars certificate")
-                .price(1000L)
-                .duration(10)
-                .createDate(LocalDateTime.parse("2022-01-03T16:30:20"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-03T16:30:20"))
-                .build();
-
-        oneHundredDollarsCertificate = GiftCertificate.builder()
-                .id(3L)
-                .name("100 dollars certificate")
-                .description("Buy anything for 100 dollars")
-                .price(10000L)
-                .duration(20)
-                .createDate(LocalDateTime.parse("2022-01-05T14:29:12"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-05T14:29:12"))
-                .build();
-
-        NoTagsCertificate = GiftCertificate.builder()
-                .id(4L)
-                .name("Awesome certificate without tags")
-                .description("No tags provided")
-                .price(100L)
-                .duration(3)
-                .createDate(LocalDateTime.parse("2022-01-10T17:19:46"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-11T12:19:46"))
-                .build();
-    }
+    private final GiftCertificateDao giftCertificateDao;
+    private final EntityManager entityManager;
 
     @Autowired
-    private GiftCertificateDao giftCertificateDao;
+    GiftCertificateDaoImplTest(GiftCertificateDao giftCertificateDao, EntityManager entityManager) {
+        this.giftCertificateDao = giftCertificateDao;
+        this.entityManager = entityManager;
+    }
+
+    private long countTotalEntities() {
+        return entityManager.createQuery("SELECT count(gc) FROM GiftCertificate gc", Long.class)
+                .getSingleResult();
+    }
 
     @Test
     @Tag("findAll")
     void shouldFindAllCertificates() {
-        List<GiftCertificate> expected = List.of(
-                cocaColaCertificate,
-                kfcCertificate,
-                oneHundredDollarsCertificate,
-                NoTagsCertificate
-        );
+        long totalEntities = countTotalEntities();
+        List<GiftCertificate> list = entityManager.createQuery("FROM GiftCertificate", GiftCertificate.class)
+                .getResultList();
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, totalEntities);
 
-        List<GiftCertificate> actual = giftCertificateDao.findAll(new Page(1));
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(DEFAULT_PAGEABLE);
 
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @Tag("findById")
-    void shouldFindByIdExistingCertificate() {
-        Optional<GiftCertificate> expected = Optional.of(oneHundredDollarsCertificate);
+    @Tag("findAll")
+    void shouldFindCertificatesWithLimitAndOffset() {
+        long totalEntities = countTotalEntities();
+        List<GiftCertificate> list = entityManager.createQuery("FROM GiftCertificate", GiftCertificate.class)
+                .setFirstResult(2)
+                .setMaxResults(2)
+                .getResultList();
+        PageRequest pageable = PageRequest.of(1, 2);
+        Page<GiftCertificate> expected = new PageImpl<>(list, pageable, totalEntities);
 
-        Optional<GiftCertificate> actual = giftCertificateDao.findById(3L);
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(pageable);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAll")
+    void shouldReturnEmptyListIfNoSuchPage() {
+        long totalEntities = countTotalEntities();
+        List<GiftCertificate> list = Collections.emptyList();
+        PageRequest pageable = PageRequest.of(15, 20);
+        Page<GiftCertificate> expected = new PageImpl<>(list, pageable, totalEntities);
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(pageable);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @Tag("findById")
-    void shouldReturnEmptyOptionalForFindByIdIfNoExistingCertificate() {
-        Optional<GiftCertificate> actual = giftCertificateDao.findById(500L);
+    void shouldFindByIdExistingCertificate() {
+        Long id = 3L;
+        Optional<GiftCertificate> expected = Optional.of(entityManager.find(GiftCertificate.class, id));
+
+        Optional<GiftCertificate> actual = giftCertificateDao.findById(id);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldReturnEmptyOptionalIfNoSuchId() {
+        Long id = 300L;
+
+        Optional<GiftCertificate> actual = giftCertificateDao.findById(id);
 
         assertThat(actual).isEmpty();
     }
@@ -113,13 +114,27 @@ class GiftCertificateDaoImplTest {
                 .description("For test only")
                 .price(500L)
                 .duration(6)
-                .createDate(LocalDateTime.parse("2022-01-16T10:19:46"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-16T10:19:46"))
                 .build();
 
         giftCertificateDao.create(newCertificate);
 
         assertThat(newCertificate.getId()).isNotNull();
+    }
+
+    @Test
+    @Tag("create")
+    void shouldSetAuditableFieldsBeforeCreation() {
+        GiftCertificate newCertificate = GiftCertificate.builder()
+                .name("New certificate")
+                .description("For test only")
+                .price(500L)
+                .duration(6)
+                .build();
+
+        giftCertificateDao.create(newCertificate);
+
+        assertThat(newCertificate.getCreateDate()).isNotNull();
+        assertThat(newCertificate.getLastUpdateDate()).isNotNull();
     }
 
     @Test
@@ -130,82 +145,207 @@ class GiftCertificateDaoImplTest {
                 .description("For test only")
                 .price(500L)
                 .duration(6)
-                .createDate(LocalDateTime.parse("2022-01-16T10:19:46"))
-                .lastUpdateDate(LocalDateTime.parse("2022-01-16T10:19:46"))
                 .build();
 
         giftCertificateDao.create(newCertificate);
-        Optional<GiftCertificate> expected = Optional.of(newCertificate);
-        Optional<GiftCertificate> actual = giftCertificateDao.findById(newCertificate.getId());
+        GiftCertificate actual = entityManager.find(GiftCertificate.class, newCertificate.getId());
 
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(newCertificate);
     }
 
     @Test
     @Tag("update")
     void shouldUpdateAllFieldsOfCertificateInDatabase() {
-        kfcCertificate.setName("KFC updated certificate");
-        kfcCertificate.setDescription("Up to date now");
-        kfcCertificate.setPrice(200L);
-        kfcCertificate.setDuration(1);
-        kfcCertificate.setCreateDate(LocalDateTime.parse("2022-01-16T15:19:46"));
-        kfcCertificate.setLastUpdateDate(LocalDateTime.parse("2022-01-16T18:19:46"));
+        Long id = 1L;
+        GiftCertificate giftCertificate = entityManager.find(GiftCertificate.class, id);
+        giftCertificate.setName("Updated certificate");
+        giftCertificate.setDescription("Up to date now");
+        giftCertificate.setPrice(200L);
+        giftCertificate.setDuration(1);
+        giftCertificate.setTags(Collections.emptyList());
 
-        giftCertificateDao.update(kfcCertificate);
-        Optional<GiftCertificate> expected = Optional.of(kfcCertificate);
-        Optional<GiftCertificate> actual = giftCertificateDao.findById(kfcCertificate.getId());
+        giftCertificateDao.update(giftCertificate);
+        GiftCertificate actual = entityManager.find(GiftCertificate.class, id);
+
+        assertThat(actual).isEqualTo(giftCertificate);
+    }
+
+    @Test
+    @Tag("update")
+    void shouldUpdateLastUpdateDateField() {
+        Long id = 1L;
+        GiftCertificate giftCertificate = entityManager.find(GiftCertificate.class, id);
+        giftCertificate.setName("Updated certificate");
+        giftCertificate.setDescription("Up to date now");
+        giftCertificate.setPrice(200L);
+        giftCertificate.setDuration(1);
+        giftCertificate.setTags(Collections.emptyList());
+
+        LocalDateTime oldLastUpdateDate = giftCertificate.getLastUpdateDate();
+
+        giftCertificateDao.update(giftCertificate);
+        entityManager.flush();
+        GiftCertificate updatedCertificate = entityManager.find(GiftCertificate.class, id);
+        LocalDateTime actual = updatedCertificate.getLastUpdateDate();
+
+        assertThat(actual).isAfter(oldLastUpdateDate);
+    }
+
+    @Test
+    @Tag("update")
+    void shouldNotUpdateCreateDateField() {
+        Long id = 1L;
+        GiftCertificate giftCertificate = entityManager.find(GiftCertificate.class, id);
+        giftCertificate.setName("Updated certificate");
+        giftCertificate.setDescription("Up to date now");
+        giftCertificate.setPrice(200L);
+        giftCertificate.setDuration(1);
+        giftCertificate.setTags(Collections.emptyList());
+
+        LocalDateTime expected = giftCertificate.getCreateDate();
+
+        giftCertificateDao.update(giftCertificate);
+        GiftCertificate updatedCertificate = entityManager.find(GiftCertificate.class, id);
+        LocalDateTime actual = updatedCertificate.getCreateDate();
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @Tag("update")
-    void shouldUpdateCertainFieldsOfCertificateInDatabase() {
-        String newName = "Very awesome cola certificate!";
-        LocalDateTime newLastUpdateDate = LocalDateTime.parse("2022-01-17T10:10:10");
-        cocaColaCertificate.setName(newName);
-        cocaColaCertificate.setLastUpdateDate(newLastUpdateDate);
-        GiftCertificate updatedCertificate = GiftCertificate.builder()
-                .id(cocaColaCertificate.getId())
-                .name(newName)
-                .lastUpdateDate(newLastUpdateDate)
+    void shouldUpdateCertainFields() {
+        Long id = 1L;
+        GiftCertificate giftCertificate = GiftCertificate.builder()
+                .id(id)
+                .description("Up to date now")
                 .build();
 
-        giftCertificateDao.update(updatedCertificate);
-        Optional<GiftCertificate> expected = Optional.of(cocaColaCertificate);
-        Optional<GiftCertificate> actual = giftCertificateDao.findById(updatedCertificate.getId());
+        giftCertificateDao.update(giftCertificate);
 
-        assertThat(actual).isEqualTo(expected);
+        GiftCertificate actual = entityManager.find(GiftCertificate.class, id);
+
+        assertThat(actual.getDescription()).isEqualTo(giftCertificate.getDescription());
     }
 
     @Test
     @Tag("delete")
     void shouldDeleteExistingCertificate() {
-        List<GiftCertificate> expected = List.of(
-                cocaColaCertificate,
-                kfcCertificate,
-                oneHundredDollarsCertificate
-        );
+        Long id = 1L;
 
-        giftCertificateDao.delete(NoTagsCertificate.getId());
-        List<GiftCertificate> actual = giftCertificateDao.findAll(new Page(1));
+        boolean isDeleted = giftCertificateDao.delete(id);
+        GiftCertificate actual = entityManager.find(GiftCertificate.class, id);
 
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+        assertThat(isDeleted).isTrue();
+        assertThat(actual).isNull();
     }
 
     @Test
     @Tag("delete")
-    void shouldNotDeleteAnythingIfNotSuchId() {
-        List<GiftCertificate> expected = List.of(
-                cocaColaCertificate,
-                kfcCertificate,
-                oneHundredDollarsCertificate,
-                NoTagsCertificate
-        );
+    void shouldReturnFalseIfNoSuchId() {
+        Long id = 100L;
 
-        giftCertificateDao.delete(5000L);
-        List<GiftCertificate> actual = giftCertificateDao.findAll(new Page(1));
+        boolean actual = giftCertificateDao.delete(id);
 
-        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+        assertThat(actual).isFalse();
+    }
+
+    @Test
+    @Tag("count")
+    void shouldCountTotalEntities() {
+        long expected = countTotalEntities();
+
+        long actual = giftCertificateDao.count();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindAllEntitiesIfNoFiltersAreGiven() {
+        List<GiftCertificate> list = entityManager.createQuery("FROM GiftCertificate", GiftCertificate.class)
+                .getResultList();
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                Collections.emptyList(), null, null, Collections.emptyList());
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, list.size());
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindByOneTag() {
+        String tagName = "extreme";
+        List<GiftCertificate> list = entityManager.createQuery(
+                        "SELECT c FROM GiftCertificate c " +
+                                "join c.tags t where t.name = :name", GiftCertificate.class)
+                .setParameter("name", tagName)
+                .getResultList();
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                List.of(tagName), null, null, Collections.emptyList());
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, list.size());
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindBySeveralTagsAndNameContains() {
+        Long id = 3L;
+        List<GiftCertificate> list = List.of(entityManager.find(GiftCertificate.class, id));
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                List.of("ski", "winter"), "rtif", null, Collections.emptyList());
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, list.size());
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindNothingIfNothingMatchesGivenFilters() {
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                List.of("ski", "winter"), "rtif", "arc", Collections.emptyList());
+        Page<GiftCertificate> expected = new PageImpl<>(Collections.emptyList(), DEFAULT_PAGEABLE, 0);
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindByFiltersAndOrderByNameDesc() {
+        List<GiftCertificate> list = entityManager.createQuery(
+                        "FROM GiftCertificate WHERE id IN (3, 4) " +
+                                "ORDER BY name DESC", GiftCertificate.class)
+                .getResultList();
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                List.of("ski", "winter"), null, null, List.of("-name"));
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, list.size());
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @Tag("findAllWithFilters")
+    void shouldFindByFiltersAndOrderByNameAsc() {
+        List<GiftCertificate> list = entityManager.createQuery(
+                        "FROM GiftCertificate WHERE id IN (3, 4) " +
+                                "ORDER BY name", GiftCertificate.class)
+                .getResultList();
+        GiftCertificateFilters emptyFilters = new GiftCertificateFilters(
+                List.of("ski", "winter"), null, null, List.of("description"));
+        Page<GiftCertificate> expected = new PageImpl<>(list, DEFAULT_PAGEABLE, list.size());
+
+        Page<GiftCertificate> actual = giftCertificateDao.findAll(emptyFilters, DEFAULT_PAGEABLE);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
