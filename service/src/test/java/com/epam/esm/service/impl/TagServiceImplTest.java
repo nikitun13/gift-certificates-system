0338b04226751a527b2d0af94c1e62c531249b76
Nikita Romanov <nikitun13@gmail.com>
2022-02-17 08:47:05 +0300

    update TagServiceImplTest

--- service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
+++ service/src/test/java/com/epam/esm/service/impl/TagServiceImplTest.java
@@ -3,26 +3,36 @@ package com.epam.esm.service.impl;
 import com.epam.esm.dao.TagDao;
 import com.epam.esm.dto.CreateTagDto;
 import com.epam.esm.dto.TagDto;
-import com.epam.esm.dto.Page;
 import com.epam.esm.entity.Tag;
-import com.epam.esm.mapper.Mapper;
-import com.epam.esm.service.TagService;
-import com.epam.esm.service.config.ServiceTestConfig;
+import com.epam.esm.mapper.TagMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
 
-@SpringBootTest(classes = ServiceTestConfig.class)
+@ExtendWith(MockitoExtension.class)
 class TagServiceImplTest {
 
-    private final TagDao dao;
-    private final TagService service;
+    @InjectMocks
+    private TagServiceImpl service;
+
+    @Mock
+    private TagDao dao;
+
+    @Mock
+    private TagMapper mapper;
 
     private final Tag cocaColaTag = new Tag(1L, "coca-cola");
     private final Tag kfcTag = new Tag(2L, "kfc");
@@ -32,35 +42,42 @@ class TagServiceImplTest {
     private final TagDto kfcTagDto = new TagDto(2L, "kfc");
     private final TagDto moneyCertificateTagDto = new TagDto(3L, "money certificate");
 
-    @Autowired
-    TagServiceImplTest(TagDao dao,
-                       Mapper<Tag, TagDto> tagDtoMapper,
-                       Mapper<Tag, CreateTagDto> createTagDtoMapper) {
-        this.dao = dao;
-        service = new TagServiceImpl(dao, tagDtoMapper, createTagDtoMapper);
-    }
-
     @Test
     @org.junit.jupiter.api.Tag("findAll")
-    void shouldReturnAllEntitiesMappedToTagDto() {
-        List<Tag> daoReturn = List.of(cocaColaTag, kfcTag, moneyCertificateTag);
-        List<TagDto> expected = List.of(cocaColaTagDto, kfcTagDto, moneyCertificateTagDto);
-        Mockito.doReturn(daoReturn)
+    void shouldDelegateToDaoFindAllAndResultToMapper() {
+        Pageable pageable = PageRequest.of(0, 20);
+        List<Tag> tags = List.of(cocaColaTag, kfcTag, moneyCertificateTag);
+        Page<Tag> page = new PageImpl<>(tags, pageable, tags.size());
+        List<TagDto> tagDtoList = List.of(cocaColaTagDto, kfcTagDto, moneyCertificateTagDto);
+        Page<TagDto> expected = new PageImpl<>(tagDtoList, pageable, tagDtoList.size());
+        doReturn(cocaColaTagDto)
+                .when(mapper)
+                .toTagDto(cocaColaTag);
+        doReturn(kfcTagDto)
+                .when(mapper)
+                .toTagDto(kfcTag);
+        doReturn(moneyCertificateTagDto)
+                .when(mapper)
+                .toTagDto(moneyCertificateTag);
+        doReturn(page)
                 .when(dao)
-                .findAll(new Page(1));
+                .findAll(pageable);
 
-        List<TagDto> actual = service.findAll(new Page(1));
+        Page<TagDto> actual = service.findAll(pageable);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     @org.junit.jupiter.api.Tag("findById")
-    void shouldFindEntityByIdAndMapItToTagDto() {
+    void shouldDelegateFindByIdToDaoAndResultToMapper() {
         Long id = kfcTag.getId();
-        Mockito.doReturn(Optional.of(kfcTag))
+        doReturn(Optional.of(kfcTag))
                 .when(dao)
                 .findById(id);
+        doReturn(kfcTagDto)
+                .when(mapper)
+                .toTagDto(kfcTag);
         Optional<TagDto> expected = Optional.of(kfcTagDto);
 
         Optional<TagDto> actual = service.findById(id);
@@ -70,73 +87,73 @@ class TagServiceImplTest {
 
     @Test
     @org.junit.jupiter.api.Tag("delete")
-    void shouldDelegateToDaoAndReturnTrue() {
-        Long existingId = kfcTag.getId();
-        Mockito.doReturn(Boolean.TRUE)
+    void shouldDelegateDeleteToDao() {
+        Long id = kfcTag.getId();
+        doReturn(true)
                 .when(dao)
-                .delete(existingId);
+                .delete(id);
 
-        boolean actual = service.delete(existingId);
+        boolean actual = service.delete(id);
 
         assertThat(actual).isTrue();
     }
 
     @Test
-    @org.junit.jupiter.api.Tag("delete")
-    void shouldDelegateToDaoAndReturnFalse() {
-        Long noSuchId = 5000L;
-        Mockito.doReturn(Boolean.FALSE)
+    @org.junit.jupiter.api.Tag("create")
+    void shouldDelegateToMapperThanCreateToDaoAndToMapperAgain() {
+        String name = moneyCertificateTag.getName();
+        Long id = moneyCertificateTag.getId();
+        CreateTagDto createTagDto = new CreateTagDto(name);
+        Tag tag = new Tag(null, name);
+        doReturn(tag)
+                .when(mapper)
+                .toTag(createTagDto);
+        doAnswer(invocation -> {
+            Tag arg = invocation.getArgument(0);
+            arg.setId(id);
+            return arg;
+        })
                 .when(dao)
-                .delete(noSuchId);
+                .create(tag);
+        doReturn(moneyCertificateTagDto)
+                .when(mapper)
+                .toTagDto(moneyCertificateTag);
 
-        boolean actual = service.delete(noSuchId);
+        TagDto actual = service.create(createTagDto);
 
-        assertThat(actual).isFalse();
+        assertThat(actual).isEqualTo(moneyCertificateTagDto);
     }
 
     @Test
-    @org.junit.jupiter.api.Tag("create")
-    void shouldCreateNewTag() {
-        String newTagName = "new tag";
-        CreateTagDto newTag = new CreateTagDto(newTagName);
-        Long newId = 4L;
-        Mockito.doAnswer(invocation -> {
-                    Tag tag = invocation.getArgument(0, Tag.class);
-                    tag.setId(newId);
-                    return null;
-                })
+    @org.junit.jupiter.api.Tag("findByName")
+    void shouldDelegateFindByNameToDaoAndMappingToMapper() {
+        String name = cocaColaTag.getName();
+        doReturn(Optional.of(cocaColaTag))
                 .when(dao)
-                .create(Tag.builder().name(newTagName).build());
-        TagDto expected = new TagDto(newId, newTagName);
+                .findByName(name);
+        doReturn(cocaColaTagDto)
+                .when(mapper)
+                .toTagDto(cocaColaTag);
+        Optional<TagDto> expected = Optional.of(cocaColaTagDto);
 
-        TagDto actual = service.create(newTag);
+        Optional<TagDto> actual = service.findByName(name);
 
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    @org.junit.jupiter.api.Tag("findByName")
-    void shouldFindExistingTagByNameAndMapToTagDto() {
-        String tagName = moneyCertificateTag.getName();
-        Mockito.doReturn(Optional.of(moneyCertificateTag))
+    @org.junit.jupiter.api.Tag("findTopTagOfUserWithTheHighestCostOfAllOrders")
+    void shouldDelegateFindTopTagToDaoAndMappingToMapper() {
+        doReturn(Optional.of(moneyCertificateTag))
                 .when(dao)
-                .findByName(tagName);
+                .findTopTagOfUserWithTheHighestCostOfAllOrders();
+        doReturn(moneyCertificateTagDto)
+                .when(mapper)
+                .toTagDto(moneyCertificateTag);
+        Optional<TagDto> expected = Optional.of(moneyCertificateTagDto);
 
-        Optional<TagDto> actual = service.findByName(tagName);
+        Optional<TagDto> actual = service.findTopTagOfUserWithTheHighestCostOfAllOrders();
 
-        assertThat(actual).isEqualTo(Optional.of(moneyCertificateTagDto));
-    }
-
-    @Test
-    @org.junit.jupiter.api.Tag("findByName")
-    void shouldReturnEmptyOptionalIfNoSuchTagName() {
-        String tagName = "no such tag name";
-        Mockito.doReturn(Optional.empty())
-                .when(dao)
-                .findByName(tagName);
-
-        Optional<TagDto> actual = service.findByName(tagName);
-
-        assertThat(actual).isEmpty();
+        assertThat(actual).isEqualTo(expected);
     }
 }
