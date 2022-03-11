18502c1b2aa71b80292f9dd69ae35ea616e8a218
Nikita Romanov <nikitun13@gmail.com>
2022-03-11 23:27:05 +0300

    update User

--- shared/src/main/java/com/epam/esm/entity/User.java
+++ shared/src/main/java/com/epam/esm/entity/User.java
@@ -3,8 +3,13 @@ package com.epam.esm.entity;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.ArrayList;
@@ -16,11 +21,25 @@ import java.util.Objects;
 public class User implements BaseEntity<Long> {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
 
     @OneToMany(mappedBy = "user",
             cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
@@ -29,15 +48,23 @@ public class User implements BaseEntity<Long> {
     public User() {
     }
 
-    public User(Long id, String username) {
+    public User(Long id, String username, String password, String firstName, String lastName,
+                List<Role> roles, List<Order> orders) {
         this.id = id;
         this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.roles = roles;
+        this.orders = orders;
     }
 
-    public User(Long id, String username, List<Order> orders) {
+    public User(Long id, String username, String password, String firstName, String lastName) {
         this.id = id;
         this.username = username;
-        this.orders = orders;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public static UserBuilder builder() {
@@ -47,6 +74,11 @@ public class User implements BaseEntity<Long> {
     public void addOrder(Order order) {
         orders.add(order);
         order.setUser(this);
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+        role.getUsers().add(this);
     }
 
     @Override
@@ -75,18 +107,53 @@ public class User implements BaseEntity<Long> {
         this.orders = orders;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return Objects.equals(id, user.id)
-                && Objects.equals(username, user.username);
+                && Objects.equals(username, user.username)
+                && Objects.equals(password, user.password)
+                && Objects.equals(firstName, user.firstName)
+                && Objects.equals(lastName, user.lastName);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, username);
+        return Objects.hash(id, username, password, firstName, lastName);
     }
 
     @Override
@@ -94,12 +161,19 @@ public class User implements BaseEntity<Long> {
         return "User{"
                 + "id=" + id
                 + ", username='" + username + '\''
+                + ", password='" + password + '\''
+                + ", firstName='" + firstName + '\''
+                + ", lastName='" + lastName + '\''
                 + '}';
     }
 
     public static class UserBuilder {
         private Long id;
         private String username;
+        private String password;
+        private String firstName;
+        private String lastName;
+        private List<Role> roles = new ArrayList<>();
         private List<Order> orders = new ArrayList<>();
 
         public UserBuilder setId(Long id) {
@@ -112,13 +186,33 @@ public class User implements BaseEntity<Long> {
             return this;
         }
 
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder setRoles(List<Role> roles) {
+            this.roles = roles;
+            return this;
+        }
+
         public UserBuilder setOrders(List<Order> orders) {
             this.orders = orders;
             return this;
         }
 
         public User build() {
-            return new User(id, username, orders);
+            return new User(id, username, password, firstName, lastName, roles, orders);
         }
     }
 }
