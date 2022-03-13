eb18a5d177db380db08badce88faee89f0ffaf1f
Nikita Romanov <nikitun13@gmail.com>
2022-03-14 01:01:19 +0300

    add Security to UserController

--- web/src/main/java/com/epam/esm/controller/UserController.java
+++ web/src/main/java/com/epam/esm/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.epam.esm.controller;
 
 import com.epam.esm.dto.CreateOrderDto;
+import com.epam.esm.dto.CreateUserDto;
 import com.epam.esm.dto.DetailedOrderDto;
+import com.epam.esm.dto.LoginUserDto;
 import com.epam.esm.dto.OrderDto;
 import com.epam.esm.dto.UserDto;
 import com.epam.esm.exception.EntityNotFoundException;
 import com.epam.esm.service.OrderService;
+import com.epam.esm.service.UserAuthenticationService;
 import com.epam.esm.service.UserService;
+import com.epam.esm.util.JwtUtil;
 import com.epam.esm.util.PaginationUtil;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,8 +19,12 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.PagedModel.PageMetadata;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,21 +42,31 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+@PreAuthorize("isAuthenticated()")
 public class UserController {
 
+    private static final String BEARER_PREFIX = "Bearer ";
+
     private final UserService userService;
+    private final UserAuthenticationService userAuthenticationService;
     private final OrderService orderService;
     private final PaginationUtil paginationUtil;
+    private final JwtUtil jwtUtil;
 
     public UserController(UserService userService,
+                          UserAuthenticationService userAuthenticationService,
                           OrderService orderService,
-                          PaginationUtil paginationUtil) {
+                          PaginationUtil paginationUtil,
+                          JwtUtil jwtUtil) {
         this.userService = userService;
+        this.userAuthenticationService = userAuthenticationService;
         this.orderService = orderService;
         this.paginationUtil = paginationUtil;
+        this.jwtUtil = jwtUtil;
     }
 
     @GetMapping
+    @PreAuthorize("hasRole(T(com.epam.esm.entity.Role).ADMIN)")
     public CollectionModel<?> findAll(Pageable pageable) {
         Page<?> page = userService.findAll(pageable)
                 .map(this::buildRepresentationModelWithLinks);
@@ -62,13 +80,35 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole(T(com.epam.esm.entity.Role).ADMIN) " +
+            "or (isAuthenticated() and principal.id == #id)")
     public RepresentationModel<?> findById(@PathVariable("id") Long id) {
         UserDto dto = userService.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(id));
         return buildRepresentationModelWithLinks(dto);
     }
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("isAnonymous() or hasRole(T(com.epam.esm.entity.Role).ADMIN)")
+    public ResponseEntity<Void> create(@RequestBody @Validated CreateUserDto createUserDto) {
+        UserDto dto = userAuthenticationService.signUp(createUserDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").build(dto.id());
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("isAnonymous() or hasRole(T(com.epam.esm.entity.Role).ADMIN)")
+    public ResponseEntity<Void> login(@RequestBody @Validated LoginUserDto loginUserDto) {
+        UserDetails user = userAuthenticationService.login(loginUserDto);
+        String jwt = jwtUtil.createJwt(user.getUsername());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, BEARER_PREFIX + jwt)
+                .build();
+    }
+
     @GetMapping("/{id}/orders")
+    @PreAuthorize("hasRole(T(com.epam.esm.entity.Role).ADMIN) " +
+            "or (isAuthenticated() and principal.id == #id)")
     public CollectionModel<?> findOrdersByUser(@PathVariable("id") Long id,
                                                Pageable pageable) {
         Page<?> page = orderService.findByUserId(id, pageable)
@@ -86,6 +126,8 @@ public class UserController {
     }
 
     @GetMapping("/{id}/orders/{orderId}")
+    @PreAuthorize("hasRole(T(com.epam.esm.entity.Role).ADMIN) " +
+            "or (isAuthenticated() and principal.id == #id)")
     public RepresentationModel<?> findDetailedOrderByUser(@PathVariable("id") Long id,
                                                           @PathVariable("orderId") Long orderId) {
         DetailedOrderDto result = orderService.findByUserIdAndId(id, orderId)
@@ -98,6 +140,8 @@ public class UserController {
     }
 
     @PostMapping(value = "/{id}/orders", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasRole(T(com.epam.esm.entity.Role).ADMIN) " +
+            "or (isAuthenticated() and principal.id == #id)")
     public ResponseEntity<Void> createOrder(@PathVariable("id") Long id,
                                             @RequestBody @Valid CreateOrderDto createOrderDto) {
         DetailedOrderDto dto = orderService.create(createOrderDto, id);
