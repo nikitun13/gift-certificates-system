9e91283fe7c47b31f0534a5d8cafca9554ca491f
Nikita Romanov <nikitun13@gmail.com>
2022-03-13 23:32:40 +0300

    update UserMapper

--- shared/src/main/java/com/epam/esm/mapper/UserMapper.java
+++ shared/src/main/java/com/epam/esm/mapper/UserMapper.java
@@ -1,11 +1,17 @@
 package com.epam.esm.mapper;
 
+import com.epam.esm.dto.CreateUserDto;
 import com.epam.esm.dto.UserDto;
 import com.epam.esm.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toUserDto(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "orders", ignore = true)
+    User toUser(CreateUserDto createUserDto);
 }
