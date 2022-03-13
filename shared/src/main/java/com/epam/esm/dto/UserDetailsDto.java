8e00b6ad56894a6537b2a1291c91c2e0f87e7a3a
Nikita Romanov <nikitun13@gmail.com>
2022-03-14 00:59:19 +0300

    add UserDetailsDto

---
+++ shared/src/main/java/com/epam/esm/dto/UserDetailsDto.java
@@ -0,0 +1,50 @@
+package com.epam.esm.dto;
+
+import com.epam.esm.entity.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record UserDetailsDto(Long id,
+                             String username,
+                             String password,
+                             String firstName,
+                             String lastName,
+                             Role role) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
