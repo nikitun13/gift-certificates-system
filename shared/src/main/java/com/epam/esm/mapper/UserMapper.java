e83d15c4bc709bfd532c85b2adb3b30586e596c2
Nikita Romanov <nikitun13@gmail.com>
2022-03-18 15:08:12 +0300

    update UserMapper

--- shared/src/main/java/com/epam/esm/mapper/UserMapper.java
+++ shared/src/main/java/com/epam/esm/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.epam.esm.mapper;
 
 import com.epam.esm.dto.CreateUserDto;
+import com.epam.esm.dto.OktaUserDto;
 import com.epam.esm.dto.UserDetailsDto;
 import com.epam.esm.dto.UserDto;
 import com.epam.esm.entity.User;
@@ -16,6 +17,12 @@ public interface UserMapper {
     @Mapping(target = "role", ignore = true)
     @Mapping(target = "orders", ignore = true)
     User toUser(CreateUserDto createUserDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "role", ignore = true)
+    @Mapping(target = "orders", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    User toUser(OktaUserDto oktaUser);
 
     UserDetailsDto toUserDetailsDto(User user);
 }
