20c766a2966902a7c5800ffeb61be55c7894a4b8
Nikita Romanov <nikitun13@gmail.com>
2022-04-29 00:22:16 +0300

    replace printStackTrace with logger

--- web/src/main/java/com/epam/esm/actuator/ServerIpInfoContributor.java
+++ web/src/main/java/com/epam/esm/actuator/ServerIpInfoContributor.java
@@ -1,5 +1,7 @@
 package com.epam.esm.actuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.stereotype.Component;
@@ -10,13 +12,15 @@ import java.net.UnknownHostException;
 @Component
 public class ServerIpInfoContributor implements InfoContributor {
 
+    private static final Logger log = LoggerFactory.getLogger(ServerIpInfoContributor.class);
+
     @Override
     public void contribute(Info.Builder builder) {
         try {
             String address = InetAddress.getLocalHost().getHostAddress();
             builder.withDetail("server.address", address);
         } catch (UnknownHostException e) {
-            e.printStackTrace();
+            log.error("Unknown Host Exception", e);
         }
     }
 }
